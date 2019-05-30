a69967160ec4201f1354fdada3f90df4c3c8083e
geoffrey woodcock <theatomproject@gmail.com>
2019-05-30 20:21:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,13 @@ function increaseRankBy(){
 }
 
 function deepsestChild(){
-  
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
 }
